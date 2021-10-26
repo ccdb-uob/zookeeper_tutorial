1a09c24e9a0ed411b02f5af006b30fe8328c3638
Daniel Schien <dschien@gmail.com>
2021-10-26 16:06:19 +0100

    make version number configurable

--- start_zk_cluster.py
+++ start_zk_cluster.py
@@ -1,9 +1,18 @@
+import logging
+import sys
+
 import fabric
 import jinja2
 import yaml
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader('etc'))
 template = jinja_environment.get_template('zoo.cfg.template')
+
+# use loggers right from the start, rather than 'print'
+logger = logging.getLogger(__name__)
+# this will log boto output to std out
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
 
 with open('config.yaml') as f:
     config = yaml.load(f)
@@ -13,20 +22,23 @@ hosts = [(k, v) for k, v in config['hosts'].items()]
 from fabric import Connection
 
 instance_count = len(hosts)
+zk_version = '3.6.3'
 
 # let range run from 1 to n!
 for idx in range(1, instance_count + 1):
 
     ip_pair = hosts[idx - 1]
     pub_ip = ip_pair[0]
+    vm_user = config['vm_user']
+    c = Connection(f'{vm_user}@{pub_ip}', connect_kwargs={'key_filename': config['ssh_path']})
 
-    c = Connection(f'ubuntu@{pub_ip}', connect_kwargs={'key_filename': config['ssh_path']})
+    # with c.cd('/home/ubuntu'):
+    with c.cd('~'):
+        c.run(f'wget https://apache.mirrors.nublue.co.uk/zookeeper/zookeeper-{zk_version}/apache-zookeeper-{zk_version}-bin.tar.gz')
 
-    with c.cd('/home/ubuntu'):
-        c.run('wget https://apache.mirrors.nublue.co.uk/zookeeper/zookeeper-3.6.2/apache-zookeeper-3.6.2-bin.tar.gz')
-        c.run('tar -xzf apache-zookeeper-3.6.2-bin.tar.gz')
+        c.run(f'tar -xzf apache-zookeeper-{zk_version}-bin.tar.gz')
 
-    c.sudo('mv /home/ubuntu/apache-zookeeper-3.6.2-bin /usr/local/zookeeper', warn=True)
+    c.sudo(f'mv ~/apache-zookeeper-{zk_version}-bin /usr/local/zookeeper', warn=True)
     c.sudo('mkdir -p /var/lib/zookeeper')
 
     file_name = f'etc/zoo_{idx}.cfg'
@@ -44,10 +56,11 @@ for idx in range(1, instance_count + 1):
 
         cfg_file.write(template.render(server_list=server_list))
 
-    c.put(local=file_name, remote="/home/ubuntu/zoo.cfg")
-    c.sudo('cp /home/ubuntu/zoo.cfg /usr/local/zookeeper/conf/zoo.cfg')
+    c.put(local=file_name, remote="zoo.cfg")
+    c.sudo('cp ~/zoo.cfg /usr/local/zookeeper/conf/zoo.cfg')
     c.sudo(f'echo {idx} | sudo tee -a /var/lib/zookeeper/myid')
     c.sudo(f'apt-get update')
+
     c.sudo(f'apt install -y openjdk-11-jdk')
 
     c.run('export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64')
